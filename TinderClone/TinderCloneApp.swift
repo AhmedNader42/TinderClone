55a4d359138ea4234f3de0134b89885736339c48
Ahmed Nader <ahmednader111@gmail.com>
2021-05-31 00:47:53 +0200

    Initial Commit

---
+++ TinderClone/TinderCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  TinderCloneApp.swift
+//  TinderClone
+//
+//  Created by ahmed on 31/05/2021.
+//
+
+import SwiftUI
+
+@main
+struct TinderCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
